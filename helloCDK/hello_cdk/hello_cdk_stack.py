5a8aa561d7b0fc21e1237862b2ba7737f9d6e992
Boris Veis <12915945+borisveis@users.noreply.github.com>
2024-07-15 14:44:14 -0700

    initial working cdk app

---
+++ helloCDK/hello_cdk/hello_cdk_stack.py
@@ -0,0 +1,20 @@
+from aws_cdk import (
+    aws_ec2 as ec2,
+    App, Stack
+)
+from constructs import Construct
+
+class HelloCdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        vpc = ec2.Vpc(self, "MyVpc",
+                      max_azs=3  # Default is all AZs in the region
+                      )
+        # Define an EC2 instance
+        instance = ec2.Instance(self, "MyInstance",
+                                instance_type=ec2.InstanceType("t2.micro"),
+                                machine_image=ec2.MachineImage.latest_amazon_linux2(),
+                                vpc=vpc
+                                )
